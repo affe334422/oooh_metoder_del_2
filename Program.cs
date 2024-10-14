5ee44bd167fd1700c71c3e450a1a47798a3f572f
affe334422 <alle07002@utb.vaxjo.se>
2024-10-14 09:09:35 +0200

    tror det är rätt, frågade aldrig men men

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a=10, b=20;
+BytPlats(ref a, ref b);
+Console.WriteLine(a+" "+b);
+
+	
+static void BytPlats(ref int a1, ref int b1){
+    int test = a1;
+    a1 = b1;
+    b1 = test;
+}
+
+
+Console.WriteLine("10 upphöjt till 7: "+Power(10,7));
+Console.WriteLine("2 upphöjt till 10: "+Power(2,10));
+
+static double Power(double a, double b){
+   double a1 = Math.Pow(a, b);
+    return a1;
+}
